4adc265ce6f4f0595cae41b2163a1b4bedfd4933
rqdsousa <rqdsousa@gmail.com>
2021-06-04 01:58:55 -0300

    Gerando o swagger

--- src/main/java/br/com/project/table/Controller/PlayersController.java
+++ src/main/java/br/com/project/table/Controller/PlayersController.java
@@ -2,6 +2,7 @@ package br.com.project.table.Controller;
 
 import br.com.project.table.Entity.PlayerEntity;
 import br.com.project.table.Service.PlayersService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,11 +21,13 @@ public class PlayersController {
     PlayersService playersService;
 
     @GetMapping({"/fixed-players"})
+    @ApiOperation(value = "Retorna todos os jogadores")
     public ResponseEntity<List> getFixedPlayers() {
         return ResponseEntity.status(HttpStatus.OK).body(playersService.getFixedPlayers());
     }
 
     @PostMapping({"/new-player"})
+    @ApiOperation(value = "Cria um novo jogador")
     public ResponseEntity postPlayer(@RequestBody PlayerEntity newPlayer) {
         List player = playersService.postPlayer(newPlayer);
 
@@ -36,6 +39,7 @@ public class PlayersController {
     }
 
     @PutMapping(value="/{id}")
+    @ApiOperation(value = "Atualiza a pontuação do jogador")
     public ResponseEntity putPointsPlayer(
             @PathVariable("id") long id,
             @RequestBody PlayerEntity playerData
@@ -44,6 +48,7 @@ public class PlayersController {
     }
 
     @DeleteMapping(path = {"/{id}"})
+    @ApiOperation(value = "Deleta um jogador")
     public ResponseEntity deletePlayer(@PathVariable long id) {
         return playersService.deletePlayer(id);
     }
--- src/main/java/br/com/project/table/TableApplication.java
+++ src/main/java/br/com/project/table/TableApplication.java
@@ -1,10 +1,28 @@
 package br.com.project.table;
 
+import br.com.project.table.Entity.PlayerEntity;
+import br.com.project.table.Repository.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TableApplication {
+public class TableApplication implements CommandLineRunner {
+
+	@Autowired
+	private PlayerRepository repository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		repository.save(new PlayerEntity("Renata   ", 120));
+		repository.save(new PlayerEntity("Fernanda ", 120));
+		repository.save(new PlayerEntity("Francisco", 120));
+		repository.save(new PlayerEntity("Boris    ", 110));
+		repository.save(new PlayerEntity("Eduardo  ", 100));
+		repository.save(new PlayerEntity("Claudia  ", 100));
+		repository.save(new PlayerEntity("Franciele", 90));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TableApplication.class, args);
